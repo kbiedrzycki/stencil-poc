887a5e615237bc36c541dbf4d90c07ee65c5d7e0
Kamil Biedrzycki <kamil.biedrzycki@airhelp.com>
2019-08-26 19:49:10 +0200

    Add sass support

--- acme-components/stencil.config.ts
+++ acme-components/stencil.config.ts
@@ -1,18 +1,22 @@
 import { Config } from '@stencil/core';
+import { sass } from '@stencil/sass';
 
 export const config: Config = {
   namespace: 'acme-components',
   outputTargets: [
     {
       type: 'dist',
-      esmLoaderPath: '../loader'
+      esmLoaderPath: '../loader',
     },
     {
-      type: 'docs-readme'
+      type: 'docs-readme',
     },
     {
       type: 'www',
-      serviceWorker: null // disable service workers
-    }
-  ]
+      serviceWorker: null, // disable service workers
+    },
+  ],
+  plugins: [
+    sass(),
+  ],
 };
